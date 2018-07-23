4a056075b4074018b3f68fc7887c88c533eeda72
Upender Chinthala <uchinthala@excentus.com>
2018-07-23 12:47:56 -0500

    initial commit

---
+++ src/main/java/com/teye/registery/service/EcommerceRegisteryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.teye.registery.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceRegisteryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceRegisteryServiceApplication.class, args);
+	}
+}
